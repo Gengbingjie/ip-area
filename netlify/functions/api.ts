a79374af3849d7ccc839682da118dafe2763ea07
Gengbingjie <18810894922@163.com>
2023-12-01 16:48:28 +0800

    geoip

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,13 +1,13 @@
 import express, { Router } from "express";
+import geoip from 'geoip-lite'
 import serverless from "serverless-http";
+
 
 const app = express();
 
 const router = Router();
 
 app.use("/api/", router.get('/', (req, res) => {
-    console.log('req')
-    console.log(req)
     let ip: string = req.headers['x-forwarded-for']?.toString() ||
         req.ip ||
         req.socket.remoteAddress || '';
@@ -15,9 +15,8 @@ app.use("/api/", router.get('/', (req, res) => {
         ip = ip.split(',')[0]
     }
     ip = ip.substr(ip.lastIndexOf(':') + 1, ip.length);
-    console.log('ip')
-    console.log(ip)
-    res.send(ip)
+    let area = geoip.lookup(ip)
+    res.send(res.json(area))
 }));
 
 export const handler = serverless(app);
